4d0bc8894e71e0513100f2f0d5da4713f677fbab
Hyemi Jeon <hjeon26@my.bcit.ca>
2023-11-05 00:44:02 -0700

    addign files to repository

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# exercise-class-9
+# Lab 2 Save to Different Branches
+## Created by Jeon
+## Lab to help save files and folders into different GitHub Branches
---
+++ written.js
@@ -0,0 +1,40 @@
+/**
+ * Arrays
+ * 
+ * always use camel case where needed
+ * 
+ * Make sure to write the question/instruction and then the answers below
+ * 
+ */
+
+/**
+ * Question 5 - concat two different arrays by using .concat() method
+ * 
+ *  
+ */
+
+
+/** initialise the variable bakery to the first array containing the following items: muffins, sugar cookies, sourdough, blackforest cake.
+ *  initialise the variable cats to the second array containing the following items: tabby, calico, turkish angora, bengal.
+ *  combine the two arrays together using .concat().
+ *  console log out the variable and write a single line comment of what will be displayed.
+ * */
+
+var bakery = ["muffins", "sugar cookies", "sourdough", "blackforest cake"];
+var cats = ["tabby", "calico", "turkish angora", "bengal"];
+ 
+var bakeryAndCats = bakery.concat(cats);
+console.log(bakeryAndCats);
+
+// [ 'muffins','sugar cookies','sourdough','blackforest cake','tabby','calico','turkish angora',bengal']
+
+
+
+
+
+
+/**
+ * Question 6 - create a 2 dimensions array that is 2-by-4. Grab some values out of this array to create a sentence using concatenation.
+ * 
+ *  
+ */
